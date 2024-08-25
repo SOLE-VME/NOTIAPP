71b5016687f14422e0d7e181fdf840ec4538f37b
Macarena Soledad Cardozo <107225716+SOLE-VME@users.noreply.github.com>
2024-08-25 20:45:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,26 @@ import { Login } from "./routes/Login";
 import Navbar from "./components/NavBar";
 import { EditarArticulo } from "./components/Articulos/EditarArticulo";
 import { ProtectedRoute } from "./contexts/ProtectedRoute";
-import {Perfiles} from "./components/Profiles/Perfiles";
+import Profile from './components/Profiles/Profile';
+import EditProfile from './components/Profiles/EditProfile';
+
 
 const App = () => {
     return (
         <Router>
-            <Navbar /> 
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Articulos />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/articles/:id" element={<VerArticulo />} />
-                <Route path="/articles/nuevo-articulo" element={ <ProtectedRoute> <CrearArticulo /> </ProtectedRoute>} />
+                <Route path="/articles/nuevo-articulo" element={<ProtectedRoute><CrearArticulo /></ProtectedRoute>} />
                 <Route path="/articles/editar/:id" element={<ProtectedRoute><EditarArticulo /></ProtectedRoute>} />
-                <Route path="/perfil" element={<Perfiles />}></Route>
-                <Route path="*" element={<Navigate to="/" />}></Route>
+                <Route path="/my-profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+                <Route path="/edit-profile/:id" element={<ProtectedRoute><EditProfile /></ProtectedRoute>} />
+                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </Router>
     );
 };
 
 export default App;
-
