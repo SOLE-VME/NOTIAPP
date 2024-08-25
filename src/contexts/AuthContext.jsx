5bf6d553f5126d24cc6924251e2c9561b12f4862
Macarena Soledad Cardozo <107225716+SOLE-VME@users.noreply.github.com>
2024-08-25 20:36:47 -0300

    cambios para rescatar el userid y poder usuarlo para algunas funciones

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -7,37 +7,44 @@ export const AuthProvider = ({ children }) => {
     isAuthenticated: false,
     username: null,
     token: null,
+    userId: null,
   });
 
   useEffect(() => {
     const token = localStorage.getItem("authToken");
     const username = localStorage.getItem("username");
-    if (token && username) {
+    const userId = localStorage.getItem("userId");
+    if (token && username && userId) {
       setAuth({
         isAuthenticated: true,
         username,
         token,
+        userId,
       });
     }
   }, []);
 
-  const login = (username, token) => {
+  const login = (username, token, userId) => {
     localStorage.setItem("authToken", token);
     localStorage.setItem("username", username);
+    localStorage.setItem("userId", userId);
     setAuth({
       isAuthenticated: true,
       username,
       token,
+      userId,
     });
   };
 
   const logout = () => {
     localStorage.removeItem("authToken");
     localStorage.removeItem("username");
+    localStorage.removeItem("userId");
     setAuth({
       isAuthenticated: false,
       username: null,
       token: null,
+      userId: null,
     });
   };
 
